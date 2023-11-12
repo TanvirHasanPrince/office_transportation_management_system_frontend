cfec65dde7c9a83b212cc66963bca24df704bb71
unknown <t.hasan.prince@gmail.com>
2023-11-13 03:27:55 +0600

    [Done]- Breadcrumb

--- src/components/ui/Contents.tsx
+++ src/components/ui/Contents.tsx
@@ -1,15 +1,36 @@
 "use client";
 
 import { Breadcrumb, Layout, Menu, theme } from "antd";
+import TMSBreastCrumb from "./TMSBreastCrumb";
 
 const { Header, Content, Footer, Sider } = Layout;
 
 const Contents = ({ children }: { children: React.ReactNode }) => {
-  return <Content style={{
-minHeight: '100vh',
-color: 'black'
+  const base = 'admin'
+  return (
+    <Content
+      style={{
+        minHeight: "100vh",
+        color: "black",
+      }}
+    >
+      <TMSBreastCrumb
+        items={[
+          {
+            label: `${base}`,
+            link: `/${base}`,
+          },
 
-  }}>{children}</Content>;
+          {
+            label: `student`,
+            link: `/${base}/student`,
+          },
+        ]}
+      />
+
+      {children}
+    </Content>
+  );
 };
 
 export default Contents;
