78d46fe46702c5a969474e1a2e97ee3a267c72b8
unknown <t.hasan.prince@gmail.com>
2023-11-12 22:55:11 +0600

    [Added]- Error Page

---
+++ src/app/error.tsx
@@ -0,0 +1,29 @@
+'use client'
+import Lottie from "lottie-react";
+import somethingWentWrongAnimation from "../app/assets/SomethingWentWrongAnimation.json";
+
+
+const ErrorPage = () => {
+   const containerStyle = {
+     display: "flex",
+     justifyContent: "center",
+     alignItems: "center",
+     height: "100vh",
+   };
+
+   const animationContainerStyle = {
+     maxWidth: "400px",
+     width: "100%",
+   };
+ return (
+   <div style={containerStyle}>
+     <Lottie
+       animationData={somethingWentWrongAnimation}
+       loop={true}
+       style={animationContainerStyle}
+     />
+   </div>
+ );
+};
+
+export default ErrorPage;
