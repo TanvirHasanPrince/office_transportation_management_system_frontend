d66e25cd643a447f74321b388b4df65a3cc5e087
unknown <t.hasan.prince@gmail.com>
2023-11-12 22:50:45 +0600

    [Done]- Added Loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+import LoadingAnimation from "@/components/LoadingAnimation";
+import React from "react";
+
+const LoadingPage = () => {
+  return (
+    <div>
+      <LoadingAnimation></LoadingAnimation>
+    </div>
+  );
+};
+
+export default LoadingPage;
---
+++ src/components/LoadingAnimation.tsx
@@ -0,0 +1,28 @@
+'use client'
+import Lottie from "lottie-react";
+import LoadingCarAnimation from "../app/assets/LoadingCarAnimation.json";
+
+const LoadingAnimation = () => {
+  const containerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  };
+
+  const animationContainerStyle = {
+    maxWidth: "400px",
+    width: "100%",
+  };
+ return (
+   <div style={containerStyle}>
+     <Lottie
+       animationData={LoadingCarAnimation}
+       loop={true}
+       style={animationContainerStyle}
+     />
+   </div>
+ );
+};
+
+export default LoadingAnimation;
